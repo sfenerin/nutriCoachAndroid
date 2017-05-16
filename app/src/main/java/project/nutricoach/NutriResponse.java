b7f9f2673c42d16f8b4086d766f97ed2fc75808a
Ana Carolina Mexia <anamexia@stanford.edu>
2017-05-16 11:48:49 -0700

    commiting

--- app/src/main/java/project/nutricoach/NutriResponse.java
+++ app/src/main/java/project/nutricoach/NutriResponse.java
@@ -3,26 +3,48 @@ import com.fatsecret.platform.services.FatsecretService;
 import com.fatsecret.platform.model.CompactFood;
 import com.fatsecret.platform.services.Response;
 
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * Created by Shawn on 5/15/2017.
  */
 
-public class NutriResponse {
+public class NutriResponse implements Callable<String> {
     String key = "8a87c3522c2c4298a57ad80a4a24354c";
     String secret = "6561cc4a8711477fbc696bd1dcfea90f";
     String url = "http://platform.fatsecret.com/rest/server.api";
-
-    public NutriResponse(){
-
+    FatSecretAPI api;
+    String input;
+    public NutriResponse(String input){
+        api = new FatSecretAPI(key, secret);
+        this.input = input;
     }
 
-    public String prcoess(String input){
-        FatsecretService service = new FatsecretService(key, secret);
-        Response<CompactFood> foodResponse = service.searchFoods(input);
-        List<CompactFood> foods = foodResponse.getResults();
-        return foods.get(0).getName() + " " + foods.get(0).getDescription();
+    private String process(String input) throws UnsupportedEncodingException, JSONException {
+
+        String response = api.getFoodItems(input).toString(2);
+        return response;
+//        FatsecretService service = new FatsecretService(key, secret);
+//        Response<CompactFood> foodResponse = service.searchFoods(input);
+//        List<CompactFood> foods = foodResponse.getResults();
+//        return foods.get(0).getName() + " " + foods.get(0).getDescription();
 //        return "";
+    }
+
+    @Override
+    public String call() {
+        try {
+            String response = process(input);
+            return response;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
