afcf134cd772fc4bc8ac3387c3a98a8c4f61e270
Shawn <sfenerin@stanford.edu>
2017-05-18 14:26:52 -0700

    Skips login for testing, stub implementation of extracting food from input, stub implementation of storing food information

--- app/src/main/java/project/nutricoach/NutriResponse.java
+++ app/src/main/java/project/nutricoach/NutriResponse.java
@@ -1,11 +1,14 @@
 package project.nutricoach;
+import android.util.Log;
+
 import com.fatsecret.platform.services.FatsecretService;
 import com.fatsecret.platform.model.CompactFood;
 import com.fatsecret.platform.services.Response;
-import android.util.Log;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONArray;
+
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -17,6 +20,7 @@ import java.util.concurrent.Callable;
 public class NutriResponse implements Callable<String> {
     String key = "8a87c3522c2c4298a57ad80a4a24354c";
     String secret = "6561cc4a8711477fbc696bd1dcfea90f";
+    String url = "http://platform.fatsecret.com/rest/server.api";
     FatSecretAPI api;
     String input;
     public NutriResponse(String input){
@@ -24,14 +28,37 @@ public class NutriResponse implements Callable<String> {
         this.input = input;
     }
 
-    public String process(String input) throws UnsupportedEncodingException, JSONException {
-        JSONArray responseArray = api.getFoodItems(input).getJSONObject("result").getJSONObject("foods").getJSONArray("food");
-        Log.d("results:", responseArray.toString(2));
-        String response = responseArray.getJSONObject(0).toString(2);
-//        JSONObject result = api.getFoodItems(input);
-//        String response = api.getFoodItems(input).toString(2);
+    private String process(String input) throws UnsupportedEncodingException, JSONException {
+        String response = "";
+
+        FoodItemExtractor foodFinder = new FoodItemExtractor();
+        if(foodFinder.foundFoodItem(input)){
+            String foodQuery = foodFinder.getFoodItem();
+            JSONObject foodInfo = getFoodInfo(foodQuery);
+            storeInfo(foodInfo);
+            response = foodInfo.toString(2);
+        } else {
+            response = "Sorry, I didn't quite get that. Could you try rephrasing or being more specific?";
+        }
         return response;
     }
+
+    private boolean storeInfo(JSONObject foodInfo){
+        return true;
+    }
+
+    private JSONObject getFoodInfo(String foodQuery) throws UnsupportedEncodingException, JSONException {
+        JSONObject foodList = api.getFoodItems(foodQuery);
+        JSONArray responseArray = api.getFoodItems(input).getJSONObject("result").getJSONObject("foods").getJSONArray("food");
+        Log.d("results", responseArray.toString(2));
+        String food_id = responseArray.getJSONObject(0).get("food_id").toString().replaceAll("\\s","");
+        return api.getFoodItem(Long.parseLong(food_id));
+    }
+
+    private boolean updateFoodInfo(){
+        return true;
+    }
+
 
     @Override
     public String call() {
