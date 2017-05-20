2583bff86b0f9a593ac7c6d1d9b7833528652b1c
Ana Carolina Mexia <anamexia@stanford.edu>
2017-05-20 14:18:59 -0700

    Added data

--- app/src/main/java/project/nutricoach/InfoActivity.java
+++ app/src/main/java/project/nutricoach/InfoActivity.java
@@ -1,8 +1,70 @@
 package project.nutricoach;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by anacarolinamexia on 5/20/17.
  */
 
-public class InfoActivity {
+public class InfoActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Button submitButton = (Button) findViewById(R.id.SubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                getUserDetails();
+            }
+        });
+
+    }
+    private void getUserDetails(){
+
+        double age= Float.parseFloat(((EditText) findViewById(R.id.ageText)).getText().toString());
+        double height= Float.parseFloat(((EditText) findViewById(R.id.heightText)).getText().toString());
+        double weight= Float.parseFloat(((EditText) findViewById(R.id.weightText)).getText().toString());
+        boolean female= ((RadioButton) findViewById(R.id.radioFemale)).isChecked();
+        RadioGroup radioButtonGroup = (RadioGroup)findViewById(R.id.radioActivity);
+        int radioButtonID = ((RadioGroup)findViewById(R.id.radioActivity)).getCheckedRadioButtonId();
+        View radioButton = radioButtonGroup.findViewById(radioButtonID);
+        int activity = radioButtonGroup.indexOfChild(radioButton);
+        double [] activityMultiplier = {0, 1, 1.2, 1.375, 1.55, 1.95};
+        double bmr;
+
+        if(!female){
+            bmr = 10 * .453592 * weight + 6.25 * 2.54 * height - 5 * age + 5;
+        }else{
+            bmr = 10 * .453592 * weight + 6.25 * 2.54 * height - 5 * age - 161;
+        }
+
+        double calories = bmr* activityMultiplier[activity];
+        double protein = .453592 * weight * (.8 + activity - 1) * .23;
+        double fat =  .25*calories / 9;
+        double carbs = .55*calories / 4;
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        User newUser = new User(getIntent().getStringExtra("USER_EMAIL"), user.getUid(), age,  female, height, weight, bmr, calories,  protein, fat,  carbs, activity);
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("users").child(user.getUid()).setValue(newUser);
+
+
+
+
+    }
 }
+
---
+++ app/src/main/java/project/nutricoach/User.java
@@ -0,0 +1,138 @@
+package project.nutricoach;
+
+
+
+/**
+ * Created by anacarolinamexia on 5/20/17.
+ */
+
+public class User {
+
+    private double age;
+    private double height;
+    private double weight;
+    private double bmr;
+    private double calories;
+    private double protein;
+    private double fat;
+    private double carbs;
+    private int activity;
+    private boolean female;
+    private String email;
+    private String uid;
+
+    public User (){
+
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getBmr() {
+        return bmr;
+    }
+
+    public void setBmr(double bmr) {
+        this.bmr = bmr;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getActivity() {
+        return activity;
+    }
+
+    public void setActivity(int activity) {
+        this.activity = activity;
+    }
+
+    public boolean isFemale() {
+        return female;
+    }
+
+    public void setFemale(boolean female) {
+        this.female = female;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    public String getUid() {
+//        return uid;
+//    }
+//
+//    public void setUid(String uid) {
+//        this.uid = uid;
+//    }
+
+    public User (String email, String uid, double age, boolean female, double height, double weight, double bmr, double calories, double protein, double fat, double carbs, int activity){
+         this.email=email;
+        this.uid =uid;
+        this.age=age;
+        this.female=female;
+        this.height= height;
+        this.weight=weight;
+        this.bmr=bmr;
+        this.calories=calories;
+        this.protein= protein;
+        this.carbs= carbs;
+        this.fat=fat;
+        this.activity= activity;
+    }
+}
