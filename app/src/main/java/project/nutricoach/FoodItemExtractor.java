0fd712a7c44c7730f4ea33cccf97923f3925a91d
Katy Shi <katys@stanford.edu>
2017-05-23 10:55:00 -0700

    working HTTP requests for wit.ai

--- app/src/main/java/project/nutricoach/FoodItemExtractor.java
+++ app/src/main/java/project/nutricoach/FoodItemExtractor.java
@@ -1,5 +1,12 @@
 package project.nutricoach;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Shawn on 5/18/2017.
  */
@@ -17,11 +24,38 @@ public class FoodItemExtractor {
     // then if we are doing this from that example, servingCount should be set to 2
     // and servingSize should be "slice"
 
-    public boolean foundFoodItem(String input){
+    public boolean foundFoodItem(String input) throws IOException {
         foodItem = input;
+        getResponse("I had "+ input);
         servingCount = 1;//placeholder
         servingSize = ""; //placeholder
         return true;
+    }
+
+    public String getResponse(String input) throws IOException {
+        String modInput = input.replace(" ","%20");
+        URL url = new URL("https://api.wit.ai/message?v=20170523&q=" + modInput);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Authorization", "Bearer D7GCTRMY2GDZORZDG5MGWEAWQSEZGYME");
+
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                (conn.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+        return "";
     }
 
 
